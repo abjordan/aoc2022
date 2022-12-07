2a07416a227b5d8bea346915040a2151a474d6dd
Alex Jordan <abjordan@gmail.com>
2022-12-06 20:44:57 -0500

    Day 06

---
+++ day06/day06.py
@@ -0,0 +1,25 @@
+from loguru import logger
+
+def part1(data):
+    for line in data:
+        for i in range(4, len(line)):
+            if len(set(line[i-4:i])) == 4:
+                return i
+    
+def part2(data):
+    for line in data:
+        for i in range(14, len(line)):
+            if len(set(line[i-14:i])) == 14:
+                return i
+    
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        with open(sys.argv[1], 'r') as infile:
+            lines = infile.read().split('\n')
+    else:
+        from aocd import lines
+    
+    logger.info("Today's input is {} lines".format(len(lines))) 
+    logger.info("Answer to part 1 is: {}".format(part1(lines)))
+    logger.info("Answer to part 2 is: {}".format(part2(lines)))
