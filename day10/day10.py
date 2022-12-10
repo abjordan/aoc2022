af7c44037f35a627b203e27869a64eec4c02742d
Alex Jordan <abjordan@gmail.com>
2022-12-10 14:09:08 -0500

    Day 10: Cathode Ray Tube

---
+++ day10/day10.py
@@ -0,0 +1,89 @@
+from loguru import logger
+from enum import Enum
+
+class Inst(Enum):
+    NOOP = "noop"
+    ADDX = "addx"
+    ADDX_OLD = "addx (2nd)"
+
+class CRT:
+
+    CRITICAL = [20, 60, 100, 140, 180, 220]
+
+    def __init__(self):
+        self.X = 1
+        self.clk = 0
+        self._inst = None
+        self._arg = None
+        self._critical_values = []
+
+    def __str__(self):
+        return f"@{self.clk}: X={self.X}  inst={self._inst} arg={self._arg}"
+
+    def tick(self):
+        self.clk += 1
+        #logger.debug(self)
+        if self.clk in self.CRITICAL:
+            self._critical_values.append( self.clk * self.X )
+
+        if self._inst is Inst.NOOP:
+            self._inst = None
+            self._arg = None
+        elif self._inst is Inst.ADDX:
+            self._inst = Inst.ADDX_OLD
+            self._arg = self._arg
+        elif self._inst is Inst.ADDX_OLD:
+            self._inst = None
+            self.X = self.X + self._arg
+            self._arg = None
+        else:
+            logger.debug("Invalid processor state! {}".format(self))
+
+        return self.clk
+    
+    def ready(self):
+        return True if self._inst is None else False
+
+    def issue(self, inst, arg):
+        self._inst = inst
+        self._arg = arg
+
+def part1(data):
+    X = 1       # initial register state
+
+    crt = CRT()
+
+    values_I_care_about = []
+    for line in data:
+        toks = line.strip().split(" ")
+        #logger.debug(" --> @{} {}".format(crt.clk, line.strip()))
+        match toks[0]:
+            case "noop":
+                crt.issue(Inst.NOOP, None)
+            case "addx":
+                crt.issue(Inst.ADDX, int(toks[1]))
+            case _:
+                logger.debug("Unknown instruction {}".format(line.strip()))
+        #logger.debug(crt)
+        timer = crt.tick()
+        
+        while not crt.ready():
+            timer = crt.tick()
+
+    logger.debug(f"At end of program, X = {crt.X}")
+    return(sum(crt._critical_values))
+
+def part2(data):
+    pass
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        with open(sys.argv[1], 'r') as infile:
+            lines = infile.read().split('\n')
+    else:
+        from aocd import lines
+    
+    logger.info("Today's input is {} lines".format(len(lines))) 
+    logger.info("Answer to part 1 is: {}".format(part1(lines)))
+    logger.info("Answer to part 2 is: {}".format(part2(lines)))
